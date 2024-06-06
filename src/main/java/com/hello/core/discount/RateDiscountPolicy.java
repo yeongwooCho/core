a4ec56173525f5ce5876800d7678ee7e59f95b82
yeongwoo <jyy0223@naver.com>
2024-06-06 15:46:14 +0900

    [feat] 정률 할인 정책 테스트 코드 작성: java main 과 JUnit 차이

--- src/main/java/com/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,18 @@
 package com.hello.core.discount;
 
+import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy {
+
+    private final int discountPercent = 10;
+
     @Override
     public int discount(Member member, int price) {
-        return 0;
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
     }
 }

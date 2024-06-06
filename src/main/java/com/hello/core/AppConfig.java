bf303802b1bf26bc4d7fbfe8742a5f19cb288341
yeongwoo <jyy0223@naver.com>
2024-06-06 17:53:55 +0900

    [refactor] AppConfig에서 interface와 implements 를 한눈에 파악 가능 및 중복 제거

--- src/main/java/com/hello/core/AppConfig.java
+++ src/main/java/com/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package com.hello.core;
 
+import com.hello.core.discount.DiscountPolicy;
 import com.hello.core.discount.FixDiscountPolicy;
+import com.hello.core.discount.RateDiscountPolicy;
+import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
 import com.hello.core.member.MemoryMemberRepository;
@@ -8,11 +11,20 @@ import com.hello.core.order.OrderService;
 import com.hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    private MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy getDiscountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }
 }

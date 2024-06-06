98e2274045bf2222895039e1d5eff67f1294ad87
yeongwoo <jyy0223@naver.com>
2024-06-06 13:29:17 +0900

    [feat] MemberRepository 회원 저장소 interface, implements 구현

---
+++ src/main/java/com/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package com.hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    // HashMap 은 동시성 이슈가 있을 수 있으므로 ConcurrentHashMap 을 사용하는 것이 좋다.
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}

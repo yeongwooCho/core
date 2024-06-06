85745c327e11b6259799d4bae97d3d67f474a231
yeongwoo <jyy0223@naver.com>
2024-06-06 13:34:25 +0900

    [feat] MemberService 회원 비지니스 로직: interface, impl 구현

---
+++ src/main/java/com/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package com.hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        this.memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return this.memberRepository.findById(memberId);
+    }
+}

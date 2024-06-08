55134b66ca93f7334ddd6ace3530582431ddde62
yeongwoo <jyy0223@naver.com>
2024-06-08 18:02:56 +0900

    [feat] 컴포넌트 스캔이 스캔 할 범위를 지정하는 기능 추가 및 테스트

--- src/main/java/com/hello/core/AutoAppConfig.java
+++ src/main/java/com/hello/core/AutoAppConfig.java
@@ -1,13 +1,18 @@
 package com.hello.core;
 
+import com.hello.core.member.MemberRepository;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(
-        type = FilterType.ANNOTATION,
-        classes = Configuration.class
-))
+@ComponentScan(
+        basePackages = "com.hello.core",
+        basePackageClasses = MemberRepository.class,
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ANNOTATION,
+                classes = Configuration.class
+        )
+)
 public class AutoAppConfig {
 }
---
+++ src/test/java/com/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,21 @@
+package com.hello.core.scan;
+
+import com.hello.core.AutoAppConfig;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}

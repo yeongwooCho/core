4a759d676e431fbba9988b6abc1b8b5e7d303dd6
yeongwoo <jyy0223@naver.com>
2024-06-09 17:58:15 +0900

    [feat] Interface InitializingBean, DisposableBean을 이용한 의존관계 관련 콜백 기능 추가

---
+++ src/test/java/com/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,42 @@
+package com.hello.core.lifecycle;
+
+import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+@Setter
+public class NetworkClient implements InitializingBean, DisposableBean {
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+}

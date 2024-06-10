5576bb8c894119f1dc438c7f6e5a9cc20fc8b50a
yeongwoo <jyy0223@naver.com>
2024-06-10 19:35:18 +0900

    [feat] scope proxy 를 이용한 request scope bean 관리

--- src/main/java/com/hello/core/web/LogDemoController.java
+++ src/main/java/com/hello/core/web/LogDemoController.java
@@ -12,13 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
-
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
